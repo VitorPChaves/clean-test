524440a377f04578e11fd627abe5a013fe122373
Vitor Patricio Chaves <vitor.patricio.chaves@gmail.com>
2025-05-08 23:47:19 -0300

    navbar details

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-6 py-4 shadow-md">
-      <div className="flex items-center gap-2 w-[200px]">
+    <nav className="flex items-center justify-between px-6 py-6 shadow-md">
+      <div className="flex items-end justify-center gap-2 w-[200px]">
         <Image
           src="/images/logo.png"
           alt=""
@@ -14,12 +14,14 @@ export function Navbar() {
           height={40}
           className="rounded-[10px]"
         />
-        <span className="text-xl font-bold text-blue-700">Lu Clean</span>
+        <span className="text-xl font-bold text-[#000] font-thin">
+          Lu Clean
+        </span>
       </div>
       <div className="space-x-4 text-sm text-gray-700">
         <Link href="/">About Us</Link>
         <Link href="/">Contact</Link>
-        <Link href="/">(XXX) XXX-XXXX</Link>
+        <Link href="/">(240) 643-0808</Link>
       </div>
     </nav>
   );
